6e4226e4d24b998f2525603f27ebd58c788f872a
Kristóf Szabó <szabokristof997@gmail.com>
2019-11-04 03:24:06 +0100

    Probakód update

---
+++ Programom/Hungary_map.xaml.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Programom
+{
+    /// <summary>
+    /// Interaction logic for Hungary_map.xaml
+    /// </summary>
+    /// 
+    public class NAryDictionary<TKey, TValue> :
+       Dictionary<TKey, TValue>
+    {
+    }
+
+    public class NAryDictionary<TKey1, TKey2, TValue> :
+        Dictionary<TKey1, NAryDictionary<TKey2, TValue>>
+    {
+    }
+
+    public class NAryDictionary<TKey1, TKey2, TKey3, TValue> :
+        Dictionary<TKey1, NAryDictionary<TKey2, TKey3, TValue>>
+    {
+    }
+
+    public class Játék
+    {
+
+    }
+
+
+
+    public partial class Hungary_map : Window
+    {
+        static NAryDictionary<int, NAryDictionary<String, bool>> Kérdések = new NAryDictionary<int, NAryDictionary<string, bool>>()
+        {1, { } };
+        static Dictionary<int, Dictionary<String, bool>> Magyarkérdések = new Dictionary<int, Dictionary<string, bool>>
+      { 1, {{"Magyarország fővárosa Budapest.", true} {"Magyarország fővárosa Debrecen.", true} }};
+
+        protected int score { get; set; }
+        
+
+        public Hungary_map()
+        {
+            InitializeComponent();
+        }
+
+        private void ShowScore()
+        {
+            Pontok.Text = "Pontok: " + score.ToString() + " pont";
+        }
+
+        private void Nyugatészak_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void Nyugatközép_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void Nyugatalsó_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void Baranya_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void Északfelső_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void Shithole_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void Jobbbészak_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void Keletnyírség_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void Keletközép_MouseLeftButtonDown_1(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void MiddleMiddle_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void Middleleft_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void Tolna_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void MiddleDél_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Programom/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Programom
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /// 
+    
+    public class Sajt
+    {
+        public string előnév;
+        public string utónév;
+        public bool id;
+
+        public string Name()
+        { return előnév + " " + utónév; }
+
+    }
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+        }
+        private void Szövegbuborék_MouseEnter(object sender, MouseEventArgs e)
+        {
+            Window1 pé = new Window1();
+            pé.Show();
+            
+        }
+    }
+}
